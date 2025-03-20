8c8bdb3fe113b506e3db66d64996abae3c69dafa
Nawin Kishore <151768353+Nawinkishore@users.noreply.github.com>
2025-03-20 12:27:13 +0530

    Update : Cloudinary

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,25 +1,28 @@
-import { v2 as cloudinary } from 'cloudinary';
-import multer from 'multer';
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // Multer configuration for memory storage
 const storage = multer.memoryStorage();
 
 async function imageUploadUtils(fileBuffer, mimetype) {
-    const result = await cloudinary.uploader.upload(`data:${mimetype};base64,${fileBuffer.toString("base64")}`, {
-        resource_type: 'auto',
-    });
-    return result;
+  const result = await cloudinary.uploader.upload(
+    `data:${mimetype};base64,${fileBuffer.toString("base64")}`,
+    {
+      resource_type: "auto",
+    }
+  );
+  return result;
 }
 
 const upload = multer({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5MB file limit
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file limit
 });
 
 export { upload, imageUploadUtils };
