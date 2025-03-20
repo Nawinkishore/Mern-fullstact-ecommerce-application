a472acf38444da59a210ef634fcf80065e85f7ac
Nawin Kishore <151768353+Nawinkishore@users.noreply.github.com>
2025-03-20 12:27:33 +0530

    Update : Product.js Model

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,39 +1,42 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    image : {
-        type : String,
-        required : true
+const productSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
     },
-    title : {
-        type : String,
-        required : true
+    title: {
+      type: String,
+      required: true,
     },
-    description : {
-        type : String,
-        required : true
+    description: {
+      type: String,
+      required: true,
     },
-    category : {
-        type : String,
-        required : true
+    category: {
+      type: String,
+      required: true,
     },
-    brand : {
-        type : String,
-        required : true
+    brand: {
+      type: String,
+      required: true,
     },
-    price : {
-        type : Number,
-        required : true
+    price: {
+      type: Number,
+      required: true,
     },
-    salePrice : {
-        type : Number,
+    salePrice: {
+      type: Number,
     },
-    totalStock : {
-        type : Number,
-        required : true
-    }
-},{timestamps : true});
+    totalStock: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
