d3909f56fa032c92783143f46dfdb8022d97ae75
Nawin Kishore <151768353+Nawinkishore@users.noreply.github.com>
2025-03-21 11:09:24 +0530

    Add Product Controller Added

--- server/controllers/admin/products-controller.js
+++ server/controllers/admin/products-controller.js
@@ -1,5 +1,5 @@
 import { imageUploadUtils } from "../../helpers/cloudinary.js";
-
+import Product from "../../models/Product.js"
 const handleImageUpload = async (req, res) => {
   try {
     if (!req.file) {
@@ -26,6 +26,40 @@ const handleImageUpload = async (req, res) => {
   }
 };
 // add a product
+
+export default addProducts = async (req,res)=>
+{
+   const {image,title,description,category,brand,price,salePrice,totalStock} = req.body;
+   try{
+      if(!image || !title || !description || !category || !brand || !price || !salePrice || !totalStock){
+         return res.status(400).json({
+            success:false,
+            message:"All fields are required"
+         });
+      }
+      const product = new Product({
+         image,
+         title,
+         description,
+         category,
+         brand,
+         price,
+         salePrice,
+         totalStock
+      });
+      await product.save();
+
+   }
+   catch(error){
+      // console.error("Add Product Error:",error);
+      res.status(500).json({
+         success:false,
+         message:"Something went wrong",
+         error:error.message
+      });
+   }
+
+}
 
 // fetch all the products
 
