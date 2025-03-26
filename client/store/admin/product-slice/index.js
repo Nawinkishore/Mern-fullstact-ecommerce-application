c4bc9279932e4871225de13a6de6e25ea67da061
Nawin Kishore <151768353+Nawinkishore@users.noreply.github.com>
2025-03-26 22:52:06 +0530

    "Store" : Admin Product Slice added

--- client/store/admin/product-slice/index.js
+++ client/store/admin/product-slice/index.js
@@ -22,7 +22,7 @@ export const fetchAllProducts = createAsyncThunk('/products/fetchAllProducts', a
 });
 
 export const editProduct = createAsyncThunk('/products/editProduct', async ({id,formData})=>{
-    const result = await axios.post(`http://localhost:5000/api/admin/products/edit/${id}`, formData,{
+    const result = await axios.put(`http://localhost:5000/api/admin/products/edit/${id}`, formData,{
         headers : {
             'Content-Type' : 'application/json'
         }
@@ -40,6 +40,18 @@ const AdminProductSlice = createSlice({
     initialState,
     reducers : {},
     extraReducers :(builder)=>{
+        builder.addCase(fetchAllProducts.pending, (state)=>{
+            state.isLoading = true;
 
+        }).addCase(fetchAllProducts.fulfilled ,(state,action)=>{
+            console.log(action.payload);
+            state.isLoading = false;
+            state.productList = action.payload;
+        }).addCase(fetchAllProducts.rejected, (state)=>{
+            state.isLoading = false;
+            state.productList = [];
+        });
     }
 })
+
+export default AdminProductSlice.reducer;
--- client/store/store.js
+++ client/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice/index.js";
+import adminProductsSlice from "./admin/product-slice/index.js";
 const store = configureStore({
     reducer : {
-        auth : authReducer
+        auth : authReducer,
+        adminProducts : adminProductsSlice,
     }
 });
 export default store;
